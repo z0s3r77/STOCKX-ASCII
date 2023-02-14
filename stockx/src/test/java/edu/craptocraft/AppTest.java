edbacae20a28416f38df21deea3206e4b87f540b
z0s3r77 <ipopdue24@gmail.com>
2023-02-14 22:51:29 +0100

    feat: Preparación del examen

---
+++ stockx/src/test/java/edu/craptocraft/AppTest.java
@@ -0,0 +1,20 @@
+package edu.craptocraft;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
