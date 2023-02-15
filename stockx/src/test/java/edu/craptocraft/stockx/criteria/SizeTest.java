5eeda3fb427891a07966dd160d04f6cba6d81fa6
z0s3r77 <ipopdue24@gmail.com>
2023-02-15 17:58:30 +0100

    refactor: SizeTest

--- stockx/src/test/java/edu/craptocraft/stockx/criteria/SizeTest.java
+++ stockx/src/test/java/edu/craptocraft/stockx/criteria/SizeTest.java
@@ -1,12 +1,10 @@
 package edu.craptocraft.stockx.criteria;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.junit.BeforeClass;
